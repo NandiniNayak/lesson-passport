7ea11db7568d38c4b1ad2e39ea99405f8ab9bd28
Nandini Nayak <nandini.r.nayak@gmail.com>
2019-10-14 14:26:48 +1100

    express boiler plate

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
-const morgan = require("morgan");
+// const morgan = require("morgan");
 const app = express();
 
 app.engine("handlebars", exphbs({defaultLayout: "main"}));
@@ -9,7 +9,7 @@ app.set("view engine", "handlebars");
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.use(morgan("combined"));
+// app.use(morgan("combined"));
 
 app.use(require("./routes"));
 
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-require("dotenv").config();
+// require("dotenv").config();
 require("./database/connect");
 const app = require('./app');
 port = 3000;
